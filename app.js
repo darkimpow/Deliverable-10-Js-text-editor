df8ffeaf4b2b981ed1e3eb0d7439273f925651a9
darkimpow <darkimpowell@gmail.com>
2022-12-05 15:06:58 -0500

    added js file

---
+++ app.js
@@ -0,0 +1,11 @@
+<script>
+function bold() { document.getElementById ('textid').style.fontWeight = bold}
+function italic(){document.getElementById ('textid').style.fontStyle = italic}
+function uppercase(){ document.getElementById ('textid').style.textTransform =uppercase}          
+function lowercase(){document.getElementById ('textid').style.textTransform =lowercase}
+function capitalize(){ document.getElementById ('textid').style.textTransform =capitalize}
+function Clear(){ document.getElementById ('textid').value =""}
+function center(){document.getElementById ('textid').style.textAlign=center}
+function right(){document.getElementById ('textid').style.textAlign=right}
+function left() { document.getElementById ('textid').style.textAlign= left}
+</script>
